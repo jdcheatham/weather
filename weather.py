78eed14dc17cfc64244f4c0e17ec6f0f4631b019
jdcheatham <jdcheatham@anderson.edu>
2017-07-20 08:10:19 -0400

    Update weather.py
    Added comments for clarity

--- weather.py
+++ weather.py
@@ -1,17 +1,19 @@
-""" Python code to read serial port
+""" Python code to read serial port and output data to file
     J. Cheahtam
     using http://www.instructables.com/id/Arduino-Python-Communication-via-USB/"""
     
 import serial
 import time
 
-arduino = serial.Serial('COM7',9600, timeout=.1)
+arduino = serial.Serial('COM7',9600, timeout=.1) """Opens the serial port"""
+
 def main():
        
 	while True:
 		data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
 		ct = time.asctime( time.localtime(time.time()) )
 		if data:
+			"""outputs the data and timestamp"""
 			print (ct[4:10],',',ct[20:24],',',ct[11:19],',',data)
 			
 			outfile=open('weather','a')
